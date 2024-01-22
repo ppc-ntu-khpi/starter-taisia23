4e79021a37897fa9534a22e82a2c40c553d8c672
taisia23 <137027004+taisia23@users.noreply.github.com>
2024-01-22 10:33:55 +0000

    Готово

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+    String quote = "“What you do speaks so loudly that I cannot hear what you say.”";
+String author = " Ralph Waldo Emerson";
+
+    public void display() {
+        System.out.println(quote+"\n\t\t"+author);
+    }
+}
